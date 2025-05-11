d7bb49f9136309386aef5f7e712ef9f4e4c6722b
Pedro Soares <pmbs.123@gmail.com>
2025-05-11 22:34:12 +0200

    feat: create/move node executables into apps/

--- apps/singlecamera_node.cpp
+++ apps/singlecamera_node.cpp
@@ -1,13 +1,9 @@
 #include <rclcpp/rclcpp.hpp>
-#include <rclcpp/utilities.hpp>
 
-#include "argus_camera_ros/camera_node.hpp"
 #include "argus_camera_ros/test_node.hpp"
 
 int main(int argc, char** argv) {
     rclcpp::init(argc, argv);
-    // std::shared_ptr<ArgusCameraNode> camera_node =
-    // std::make_shared<ArgusCameraNode>();
     std::shared_ptr<TestNode> camera_node = std::make_shared<TestNode>();
 
     rclcpp::spin(camera_node);
